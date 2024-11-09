2230a262c5c206e871008ff410b2b7af54b1dfda
k1R1LL17 <stasyanravluk@gmail.com>
2024-11-09 19:48:27 +0100

    Project

---
+++ CUDA/MultipyMatrixesCUDA.cu
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <cuda_runtime.h>
+
+__global__ void multiplyMatrices(int* firstMatrix, int* secondMatrix, int* resultMatrix, int sizeOfMatrix) {
+    int elementIndex = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (elementIndex < sizeOfMatrix) {
+        resultMatrix[elementIndex] = firstMatrix[elementIndex] * secondMatrix[elementIndex];
+    }
+}
+
+int main() {
+    int matrixSize = 4096;
+    int memorySize = matrixSize * sizeof(int);
+
+    int* hostMatrixA = (int*)malloc(memorySize);
+    int* hostMatrixB = (int*)malloc(memorySize);
+    int* hostResultMatrix = (int*)malloc(memorySize);
+
+    srand(time(0));
+
+    for (int elementIndex = 0; elementIndex < matrixSize; elementIndex++) {
+        hostMatrixA[elementIndex] = rand() % 100 + 1;
+        hostMatrixB[elementIndex] = rand() % 100 + 1;
+    }
+
+    int* deviceMatrixA, * deviceMatrixB, * deviceResultMatrix;
+    cudaMalloc((void**)&deviceMatrixA, memorySize);
+    cudaMalloc((void**)&deviceMatrixB, memorySize);
+    cudaMalloc((void**)&deviceResultMatrix, memorySize);
+
+    cudaMemcpy(deviceMatrixA, hostMatrixA, memorySize, cudaMemcpyHostToDevice);
+    cudaMemcpy(deviceMatrixB, hostMatrixB, memorySize, cudaMemcpyHostToDevice);
+
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (matrixSize + threadsPerBlock - 1) / threadsPerBlock;
+    multiplyMatrices << <blocksPerGrid, threadsPerBlock >> > (deviceMatrixA, deviceMatrixB, deviceResultMatrix, matrixSize);
+
+    cudaMemcpy(hostResultMatrix, deviceResultMatrix, memorySize, cudaMemcpyDeviceToHost);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "GPU Time: " << duration.count() << " seconds" << std::endl;
+
+    std::cout << "First 10 elements of the result matrix:" << std::endl;
+    for (int elementIndex = 0; elementIndex < 10; elementIndex++) {
+        std::cout << hostResultMatrix[elementIndex] << " ";
+    }
+    std::cout << std::endl;
+
+    cudaFree(deviceMatrixA);
+    cudaFree(deviceMatrixB);
+    cudaFree(deviceResultMatrix);
+    free(hostMatrixA);
+    free(hostMatrixB);
+    free(hostResultMatrix);
+
+    return 0;
+}
---
+++ SecondTask/MultipyMatrixesWindowsAPI.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <random>
+#include <Windows.h>
+
+struct ThreadData {
+    int* firstMatrix;
+    int* secondMatrix;
+    int* resultMatrix;
+    int startIndex;
+    int endIndex;
+};
+
+DWORD WINAPI multiplyMatrixPart(LPVOID lpParam) {
+    ThreadData* data = (ThreadData*)lpParam;
+    for (int elementIndex = data->startIndex; elementIndex < data->endIndex; ++elementIndex) {
+        data->resultMatrix[elementIndex] = data->firstMatrix[elementIndex] * data->secondMatrix[elementIndex];
+    }
+    return 0;
+}
+
+int main() {
+    int matrixSize = 4096;
+    int memorySize = matrixSize * sizeof(int);
+
+
+    int* hostMatrixA = (int*)malloc(memorySize);
+    int* hostMatrixB = (int*)malloc(memorySize);
+    int* hostResultMatrix = (int*)malloc(memorySize);
+
+    srand(time(0));
+
+    for (int elementIndex = 0; elementIndex < matrixSize; elementIndex++) {
+        hostMatrixA[elementIndex] = rand() % 100 + 1;
+        hostMatrixB[elementIndex] = rand() % 100 + 1;
+    }
+
+    int numberOfThreads = 16;
+    int chunkSize = matrixSize / numberOfThreads;
+    HANDLE* threadHandles = new HANDLE[numberOfThreads];
+    ThreadData* threadDataArray = new ThreadData[numberOfThreads];
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int threadIndex = 0; threadIndex < numberOfThreads; threadIndex++) {
+        threadDataArray[threadIndex].firstMatrix = hostMatrixA;
+        threadDataArray[threadIndex].secondMatrix = hostMatrixB;
+        threadDataArray[threadIndex].resultMatrix = hostResultMatrix;
+        threadDataArray[threadIndex].startIndex = threadIndex * chunkSize;
+        threadDataArray[threadIndex].endIndex = (threadIndex == numberOfThreads - 1) ? matrixSize : (threadIndex + 1) * chunkSize;
+
+        threadHandles[threadIndex] = CreateThread(
+            NULL,
+            0,
+            multiplyMatrixPart,
+            &threadDataArray[threadIndex],
+            0,
+            NULL
+        );
+    }
+
+    WaitForMultipleObjects(numberOfThreads, threadHandles, TRUE, INFINITE);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "CPU with Windows API Time: " << duration.count() << " seconds" << std::endl;
+
+    std::cout << "First 10 elements of the result matrix:" << std::endl;
+    for (int elementIndex = 0; elementIndex < 10; elementIndex++) {
+        std::cout << hostResultMatrix[elementIndex] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int threadIndex = 0; threadIndex < numberOfThreads; threadIndex++) {
+        CloseHandle(threadHandles[threadIndex]);
+    }
+    delete[] threadHandles;
+    delete[] threadDataArray;
+    free(hostMatrixA);
+    free(hostMatrixB);
+    free(hostResultMatrix);
+
+    return 0;
+}
